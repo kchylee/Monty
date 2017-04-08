aa1ac696f709f9cd126c1655b7cddc9ffebfa8e5
Ken Lee <kchylee@gmail.com>
2017-04-08 23:03:16 +0000

    Switched host and path to obtain the monty python script

--- https_example.js
+++ https_example.js
@@ -1,15 +1,15 @@
 var https = require('https');
 
 var options = {
-  host: 'www.example.com',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 var callback = function(response){
   console.log("In response handler callback!");
 
   response.on('data', function(chunk){
-    console.log('[-- CHUNK OF LENGTH] ' + chunk.length + ' --]');
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
     console.log(chunk.toString());
   });
 }
