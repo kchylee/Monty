5b2540052a50e4e36045880c055c1ab2ff069ec2
Ken Lee <kchylee@gmail.com>
2017-04-08 22:54:04 +0000

    Added response to callback function

--- https_example.js
+++ https_example.js
@@ -1,12 +1,13 @@
 var https = require('https');
 
 var options = {
-  host: 'www.example.org',
+  host: 'www.example.com',
   path: '/'
 };
 
-var callback = function(){
+var callback = function(response){
   console.log("In response handler callback!");
+  console.log("Response: ", response)
 }
 
 console.log("I'm about to make the request!");
